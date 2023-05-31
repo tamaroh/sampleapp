d2cef5756c626c80ec5c7b808f7747b796301d95
Tamaroh <tamaroh@gmail.com>
2023-05-31 11:12:45 +0900

    worked on local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:4000/api", { method: "GET" })
+      .then((res) => res.json())
+      .then((res) => setData(res.server))
+      .catch((error) => console.log(error));
+  }, []);
+  return data;
 }
 
 export default App;
